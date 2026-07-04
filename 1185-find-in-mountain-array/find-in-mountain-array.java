87d4865b206545b507f337d3bf8d4fa760a583a2
Paraji Holkar <102429816+parajiholkar@users.noreply.github.com>
2026-07-04 22:40:56 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (80.85%) - LeetSync

--- 1185-find-in-mountain-array/find-in-mountain-array.java
+++ 1185-find-in-mountain-array/find-in-mountain-array.java
@@ -44,19 +44,19 @@ class Solution {
         while(start<=end){
             int mid = start + ( end - start) / 2;
 
+            if(mountainArr.get(mid) == target){
+                    return mid;
+            }
+
             if(isAsc){
                 if(mountainArr.get(mid) > target){
                     end = mid - 1;
-                }else if(mountainArr.get(mid) == target){
-                    return mid;
                 }else{
                     start = mid + 1;
                 }
             }else{
                 if(mountainArr.get(mid) < target){
                     end = mid - 1;
-                }else if(mountainArr.get(mid) == target){
-                    return mid;
                 }else{
                     start = mid + 1;
                 }
